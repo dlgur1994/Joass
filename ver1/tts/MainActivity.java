48afd2f8df21392b31fe132a10295fb7069f1583
dlgur1994 <38805855+dlgur1994@users.noreply.github.com>
2018-08-14 16:58:12 +0900

    Create MainActivity.java

---
+++ ver1/tts/MainActivity.java
@@ -0,0 +1,61 @@
+package com.example.hyuk.practice;
+
+import android.os.Bundle;
+import android.speech.tts.TextToSpeech;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import java.util.Locale;
+
+import static android.speech.tts.TextToSpeech.ERROR;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextToSpeech tts;              // TTS 변수 선언
+    private EditText editText;
+    private Button button;
+
+    public MainActivity() {
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        editText = (EditText) findViewById(R.id.editText);
+        button = (Button) findViewById(R.id.button);
+
+        // TTS를 생성하고 OnInitListener로 초기화 한다.
+        tts = new TextToSpeech(this, new TextToSpeech.OnInitListener() {
+            @Override
+            public void onInit(int status) {
+                if(status != ERROR) {
+                    // 언어를 선택한다.
+                    tts.setLanguage(Locale.KOREAN);
+                }
+            }
+        });
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // editText에 있는 문장을 읽는다.
+                tts.speak(editText.getText().toString(),TextToSpeech.QUEUE_FLUSH, null);
+            }
+        });
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        // TTS 객체가 남아있다면 실행을 중지하고 메모리에서 제거한다.
+        if(tts != null){
+            tts.stop();
+            tts.shutdown();
+            tts = null;
+        }
+    }
+}
