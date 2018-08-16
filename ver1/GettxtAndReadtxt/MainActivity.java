82ee1caa945b1e2a97fbf40796a83ed7ae97848e
dlgur1994 <38805855+dlgur1994@users.noreply.github.com>
2018-08-16 12:47:45 +0900

    Create MainActivity.java

---
+++ ver1/GettxtAndReadtxt/MainActivity.java
@@ -0,0 +1,98 @@
+package com.example.hyuk.gettxtandreadtxt;
+
+import android.content.ContentValues;
+import android.os.AsyncTask;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import android.speech.tts.TextToSpeech;
+import android.view.View;
+import android.widget.Button;
+
+import java.util.Locale;
+
+import static android.speech.tts.TextToSpeech.ERROR;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView tv_outPut;
+    private TextToSpeech tts;              // TTS 변수 선언
+    private Button button;
+
+    String url = "http://ec2-13-209-47-170.ap-northeast-2.compute.amazonaws.com/new.php";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // 위젯에 대한 참조.
+        tv_outPut = (TextView) findViewById(R.id.tv_outPut);
+        button = (Button) findViewById(R.id.button);
+
+        // AsyncTask를 통해 HttpURLConnection 수행.
+        NetworkTask networkTask = new NetworkTask(url, null);
+        networkTask.execute();
+
+        // TTS를 생성하고 OnInitListener로 초기화 한다.
+        tts = new TextToSpeech(this, new TextToSpeech.OnInitListener() {
+            @Override
+            public void onInit(int status) {
+                if (status != ERROR) {
+                    // 언어를 선택한다.
+                    tts.setLanguage(Locale.KOREAN);
+                }
+            }
+        });
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // editText에 있는 문장을 읽는다.
+                tts.speak(tv_outPut.getText().toString(), TextToSpeech.QUEUE_FLUSH, null);
+            }
+        });
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        // TTS 객체가 남아있다면 실행을 중지하고 메모리에서 제거한다.
+        if (tts != null) {
+            tts.stop();
+            tts.shutdown();
+            tts = null;
+        }
+    }
+
+    public class NetworkTask extends AsyncTask<Void, Void, String> {
+
+        private String url;
+        private ContentValues values;
+
+        public NetworkTask(String url, ContentValues values) {
+            this.url = url;
+            this.values = values;
+        }
+
+        @Override
+        protected String doInBackground(Void... params) {
+
+            String result; // 요청 결과를 저장할 변수.
+            RequestHttpURLConnection requestHttpURLConnection = new RequestHttpURLConnection();
+            result = requestHttpURLConnection.request(url, values); // 해당 URL로 부터 결과물을 얻어온다.
+
+            return result;
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+
+            //doInBackground()로 부터 리턴된 값이 onPostExecute()의 매개변수로 넘어오므로 s를 출력한다.
+            tv_outPut.setText(s);
+        }
+
+    }
+}
