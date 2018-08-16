534de3c7093d569742b5ae36f8032d5ccbfb490e
dlgur1994 <38805855+dlgur1994@users.noreply.github.com>
2018-08-16 11:18:47 +0900

    Update MainActivity.java

--- ver1/tts/MainActivity.java
+++ ver1/tts/MainActivity.java
@@ -17,9 +17,6 @@ public class MainActivity extends AppCompatActivity {
     private EditText editText;
     private Button button;
 
-    public MainActivity() {
-    }
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
